a02e7d8fa20ced84377c2f0c9bd37ea7f66739cd
Ali Mohammadi <ali8mohammadi7@gmaiil.com>
2025-05-14 10:42:48 +0330

    created all part insted tasks area

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+let linesEffects = document.querySelectorAll(".title-main__h3")
+let asideTag = document.querySelector(".manage")
+let manageFirstPart = document.querySelector('.manage__add-folder')
+let manageSecondPart = document.querySelector(".manage__all-folder")
+
 window.addEventListener("load", () =>{
-    console.log("loged")
+    asideTag.classList.add("manage--active")
+    setTimeout(() => {
+        manageFirstPart.classList.add("active--visible")
+        manageSecondPart.classList.add("active--visible")
+
+    }, 1000);
+    setTimeout(() => {
+        linesEffects.forEach(line => {
+            line.classList.add("title-main__h3--js-activate")
+        });
+    }, 2000);
 })
